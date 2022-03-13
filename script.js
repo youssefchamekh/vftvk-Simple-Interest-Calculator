1c99636eaf9e56166165ed1b1e105edc14c5ee56
YoussefChamekh <Youssef.chamekh98@gmail.com>
2022-03-13 16:07:15 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
-        
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal <= 0) {
+        alert('Enter a positive number.');
+    }
+    else {
+        document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+
+    }
+
+
+
+}
